dd73c0abc84571b8ae7ae38f4be907638c4aa02d
Hrutik Chaudhari <hrutikchaudhari05@gmail.com>
2025-09-20 21:13:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let button = document.querySelector('input');
+
+function removeColor() {
+	let dropdown = document.getElementById('colorSelect');
+	dropdown.remove(dropdown.selectedIndex);
+}
+
+button.addEventListener("click", removeColor);
+
